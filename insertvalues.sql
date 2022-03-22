798a145cf687a16c99da7d682b9de3d01679ea98
Harpreet Kaur <59807874+HarpreetKaurNait@users.noreply.github.com>
2022-03-22 15:28:07 -0600

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -1,5 +1,4 @@
 INSERT INTO table1(firstname, rollnumber) VALUES ('Tom',1);
-INSERT INTO table2 (firstname, rollnumber) VALUES ('Harry',2);
+INSERT INTO table1 (firstname, rollnumber) VALUES ('Harry',2);
 INSERT INTO table2 (firstname, rollnumber) VALUES ('Steve',3);
 INSERT INTO table1 (firstname, rollnumber) VALUES ('Roy',4);
-INSERT INTO table1 (firstname, rollnumber) VALUES ('Tim',5);
